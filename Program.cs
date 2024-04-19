443f6cf82e5646360e0da771c33b9002ffcd31e9
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-19 14:02:53 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_oop_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
