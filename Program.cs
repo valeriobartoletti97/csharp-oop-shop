094d0001bbced81b7655f51c7e3d40af5dd35798
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-19 15:13:28 +0200

    added random code for products

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace csharp_oop_shop
 {
-    internal class Program
+     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Product newProduct = new Product();
+        }
+    }
+    public class Product
+    {
+
+        //VARIABILI E PROPRIETA
+        private int code;
+        public int Code { get { return code; } }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+        public int iva { get; set; }
+
+        //setto il codice automaticamente
+        private int SetCode()
+        {
+            System.Random rnd = new System.Random();
+            int number = rnd.Next(0, 1001);
+            return number;
+        }
+
+        public Product()
+        {
+            this.code = SetCode();
+            Console.WriteLine($"Il codice prodotto è {code}");
         }
     }
 }
